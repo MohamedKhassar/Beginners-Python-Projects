a3089ac11ee861ab428b4bbbfbe83b582cde159b
Mohamed Khassar <mohamedkhassar775@gmail.com>
2025-08-08 17:55:02 +0100

    Refactor number guessing game to allow custom range and limit of guesses

--- number_gessing_game.py
+++ number_gessing_game.py
@@ -1,19 +1,37 @@
 from random import randint
 
 def number_guessing_game():
-    picked_number=randint(1,100)
+    limit_guesses=int(input("Number of guesses: "))
     while True:
         try:
-            answer=int(input("Guess a number between 1 and 100: "))
+            min=int(input("minimum value: "))
+            max=int(input("maximum value: "))
+            if min<max:
+                picked_number=randint(min,max)
+                break
+            else:
+                print("minimum value must less than maximum value")
+        except Exception:
+           print("Please enter a valid value")
+            
+    tries=0
+    i=0
+    while i<limit_guesses:
+        try:
+            answer=int(input(f"Guess a number between {min} and {max}: "))
+            tries+=1 
             if answer>picked_number:
                 print("Too high!")
             elif answer < picked_number:
                 print("Too low!")
             else:
-                print("Congratulations! You guessed the number 🎉🎊.")
+                print(f"Congratulations! You guessed the number {picked_number} in {tries} {"tries" if tries>1 else "try"} 🎉🎊.")
                 break
+            i+=1
         except Exception:
                print("Please enter a valid number")
+    else:
+        print("You've reached your limit guesses")
 
                 
 number_guessing_game()
