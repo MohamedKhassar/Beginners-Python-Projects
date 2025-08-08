e10f6c1fa664364657f85b288a6bc835b88d7924
Mohamed Khassar <mohamedkhassar775@gmail.com>
2025-08-08 23:38:26 +0100

    refactor rock paper scissors game to modularize code

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,42 +1,86 @@
 from random import choice
 
 choices = {"r": "🪨", "p": "📃", "s": "✂️"}
+computer_score = 0
+player_score = 0
+
+
+def ask_to_play():
+    while True:
+        player_choice = str(input("Rock, paper, or scissors? (r/p/s): "))
+        if player_choice not in choices:
+            print("Invalid choice! Please choose r, p, or s.")
+            continue
+        else:
+            return player_choice
+
+
+def print_choices(player_choice, computer_choice):
+    print(f"""
+    You chose: {choices[player_choice]}
+    Computer chose: {choices[computer_choice]}
+    """)
+
+
+def check_winner(player_choice, computer_choice):
+    global player_score
+    global computer_score
+    if (computer_choice == "r" and player_choice == "s") \
+            or (computer_choice == "p" and player_choice == "r")\
+            or (computer_choice == "s" and player_choice == "p"):
+
+        # Computer wins
+        print("You lost 😥")
+
+        # Increment computer score
+        computer_score += 1
+
+    # Check if draw
+    elif computer_choice == player_choice:
+        print("Draw 🤝")
+
+    # Check if the player wins
+    else:
+        # Player wins
+        print("You Won 🎉")
+        # Increment player score
+        player_score += 1
+
+    # Print scores
+    print_scores(computer_score, player_score)
+
+
+def print_scores(computer_score, player_score):
+    print(f"""
+    Computer {computer_score} - {player_score} Player
+    """)
 
 
 def rock_paper_scissors():
-    player_score = 0
-    computer_score = 0
     while True:
+
+        # Computer choice
         computer_choice = choice(list(choices.keys()))
-        player_choice = str(input("Rock, paper, or scissors? (r/p/s): "))
-        if player_choice in list(choices.keys()):
-            print(f"""
-            You chose {choices[player_choice]}
-            Computer chose: {choices[computer_choice]}
-            """)
-            if (computer_choice == "r" and player_choice == "s") or (computer_choice == "p" and player_choice == "r") or (computer_choice == "s" and player_choice == "p"):
-                print("You lost 😥")
-                computer_score += 1
-            elif computer_choice==player_choice:
-                print("Draw 🤝")
+
+        #  Player choice
+        player_choice = ask_to_play()
+
+        # Print choices
+        print_choices(player_choice, computer_choice)
+
+        # Check if the computer wins
+        check_winner(player_choice, computer_choice)
+
+        while True:
+            ask_to_continue = input(
+                "Do you want to continue? (y/n): ").lower()
+            if ask_to_continue == "y":
+                break
+            elif ask_to_continue == "n":
+                print("Thanks for playing!")
+                return 0
             else:
-                print("You Won 🎉")
-                player_score += 1
-            print(f"""
-            computer {computer_score} - {player_score} player
-            """)
-            while True:
-                ask_to_continue = input(
-                    "Do you want to continue? (y/n): ").lower()
-                if ask_to_continue == "y":
-                    break
-                elif ask_to_continue == "n":
-                    print("Thanks for playing!")
-                    return 0
-                else:
-                    print("Invalid Answer!")
-        else:
-            print("Invalid Choice!")
+                print("Invalid Answer!")
 
 
 rock_paper_scissors()
